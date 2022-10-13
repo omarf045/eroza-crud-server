167bc4b68d37c637cde124ffc6bdea364b5521e6
omarf045 <pulido.morales.omar@gmail.com>
2022-10-13 18:12:28 -0500

    listo 2

--- database/db.js
+++ database/db.js
@@ -4,6 +4,7 @@ import { DB_HOST, DB_NAME, DB_USER, DB_PASSWORD, DB_PORT } from "../config";
 
 const db = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
+  port: DB_PORT,
   dialect: "mysql",
 });
 
